2559153a0de59b17b5171c43e87798a49db2ed59
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-08 09:33:33 -0500

    add teachers and assistance request tables

--- 3_queries_group-by/Total_Assignment_Submissions.sql
+++ 3_queries_group-by/Total_Assignment_Submissions.sql
@@ -4,3 +4,4 @@ JOIN students ON students.cohort_id  = cohorts.id
 JOIN assignment_submissions ON students.id = assignment_submissions.student_id
 GROUP BY cohorts.name
 ORDER BY count(assignment_submissions.*) DESC ;
+
