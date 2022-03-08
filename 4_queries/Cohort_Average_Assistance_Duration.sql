5fd7725e7fa0f6a536edaf3891d7188f81cc2dad
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-08 14:07:10 -0500

    add BootcampX Queries 4

---
+++ 4_queries/Cohort_Average_Assistance_Duration.sql
@@ -0,0 +1,9 @@
+Select sum(total_duration)
+FROM(
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY sum(completed_at-started_at)) as total_duration;
+
